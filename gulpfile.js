8449edc452258490cf59c4063718d74cec9081b9
lipeprado <fi-lipe2010@hotmail.com>
2017-02-08 15:12:38 -0200

    Configurando Ambiente

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify'),
     plumber = require('gulp-plumber'),
-    livereload = require('gulp-livereload');
+    liveServer = require("live-server"),
+     cssnano = require('gulp-cssnano');
 
 
 
@@ -20,11 +21,28 @@ gulp.task('styles', function(){
     gulp.src('resources/css/main.scss')
     .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest('resources/css/final'))
-    .pipe(livereload());
+    .pipe(cssnano())
+    .pipe(gulp.dest('resources/css/final'));
 
 });
 
+gulp.task('html', function(){
+
+    var params = {
+    port: 4040, // Set the server port. Defaults to 8080.
+    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    root: "", // Set root directory that's being served. Defaults to cwd.
+    open: true, // When false, it won't load your browser by default.
+    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+    file: "index.html", // When set, serve this file for every 404 (useful for single-page applications)
+    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+    mount: [['/components', './node_modules']], // Mount a directory to a route.
+    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+    middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+liveServer.start(params);
+
+});
 
 
 //Watch Task
@@ -36,4 +54,4 @@ gulp.task('watch', function(){
 
 });
 
-gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task('default', ['scripts', 'styles', 'watch','html']);
